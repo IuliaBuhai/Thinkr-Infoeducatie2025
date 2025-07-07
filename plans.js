7cbc91ca654ba4b3dfdf56a4e8416dc8dfffa742
IuliaBuhai <137499884+IuliaBuhai@users.noreply.github.com>
2025-07-07 23:24:55 +0300

    adaugare functii noi

--- plans.js
+++ plans.js
@@ -1,5 +1,5 @@
 import { auth, db } from './firebase.js';
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+import { onAuthStateChanged, addDoc, collection} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 
 onAuthStateChanged(auth, async (user) =>{
     if(!user){
@@ -28,8 +28,63 @@ form.addEventListener("submit", async (e)=> {
         hours : hours? Number(hours) : null
 
     };
-   
+   loading.style.display = "block";
+    output.innerHTML = "";
     console.log("User input", formData)
     
 
+    try{
+        const response = await fetch ("./netlify/functions/generatePlan",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(formData)
+
+        });
+
+        const result = await response.json();
+
+        if(!response.ok) throw new Error(result.error || "A apărut o eroare");
+
+
+        await addDoc(collection(db, "studyPlans"),{
+        ...formData,
+        userId:user.uid,
+        createdAt: new Date(),
+        plan:result.lan
+
+        });
+
+
+        dislayFormattedPlan(result.plan);
+    }catch(err){
+        output.innerHTML=`<p style="color:red;">${err.message}</p>`
+    }finally{
+        loading.style.display="none";
+    }
+    function dislayFormattedPlan(plan){
+        output.textContent="";
+
+        plan.forEach(day => {
+            const dayDiv= document.createElement("div");
+            dayDiv.innerHTML= `<h3>Ziua ${day.day}</h3>`
+
+            const ul = document.createElement("ul")
+
+            day.tasks.forEach(task => {
+
+                const li= document.createElement("li");
+                li.innerHTML=`
+                 <strong>${task.title}</strong>:${task.description}
+                 <em>(${task.duration} min)</em>
+                `;
+                ul.appendChild(li);
+            });
+            dayDiv.appendChild(ul);
+            output.appendChild(dayDiv);
+
+
+        }); 
+    }
 });
