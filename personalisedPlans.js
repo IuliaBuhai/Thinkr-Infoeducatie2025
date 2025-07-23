cf6c23f27017d6300e13618c5b1cf0678f5bbac9
IuliaBuhai <137499884+IuliaBuhai@users.noreply.github.com>
2025-07-23 12:42:34 +0300

    Add files via upload

---
+++ personalisedPlans.js
@@ -0,0 +1,104 @@
+import { auth, db } from './firebase.js';
+import { onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+import { addDoc, collection, query, where, getDocs} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
+
+let currentUser;
+
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    return window.location.href = 'login.html';
+  }
+  currentUser = user;
+  username = await getUserName();
+
+  initPrefForm();
+
+});
+
+async function getUserName(){
+  const userQuery = query(
+    collection(db, "users"),
+    where("userId", "==", currentUser.uid),
+  )
+  const snapshot = await getDocs(userQuery);
+
+  return snapshot.docs[0].data().username;
+}
+
+async function averageStudySessions(){
+  const sessionsQuery = query(
+    collection(db, "studySessions"),
+    where("userId", "==", currentUser.uid),
+    where("seconds", ">=", "60")
+  )
+  let secondsSum = 0;
+  let count= 0;
+  const snapshot = await getDocs(sessionsQuery);
+  snapshot.forEach(doc => {
+    const data = doc.data();
+    secondsSum+= data.seconds;
+    count++;
+  });
+
+  const avgMinutes = (secondsSum/count)/60;
+  return avgMinutes;
+}
+
+async function displayPrefForm(){
+  const form = document.getElementById("prefForm");
+  form.style.display = "block";
+
+}
+
+async function getLearnerType(){
+  const userQuery = query(
+    collection(db,"testResults"),
+    where("userId", "==", currentUser.uid)
+  )
+
+  const snapshot = await getDocs(userQuery);
+
+  const data = snapshot.docs[0].data();
+  const result = data.results;
+  return Math.max(Math.max(result["Auditiv"], result["Vizual"]), result["Tactil"])
+
+}
+async function initPrefForm() {
+ 
+  const prefQuery = query(
+    collection(db, "preferences"),
+    where("userId", "==", currentUser.uid)
+  );
+  const snapshot = await getDocs(prefQuery);
+
+  if (!snapshot.empty) {
+    
+    return;
+  }
+
+  
+  displayPrefForm();
+
+  const form = document.getElementById("prefForm");
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const age = document.getElementById("age").value.trim();
+    const avgStudy = await averageStudySessions();
+    const preferedTime = document.getElementById("preferedTime").value.trim();
+    const learnerType = await getLearnerType();
+    const tasks = document.getElementById("tasks").value.trim();
+
+    await addDoc(collection(db, "preferences"), {
+      userId: currentUser.uid,
+      age: age,
+      avgStudySession: avgStudy,
+      preferedTime: preferedTime,
+      learnerType: learnerType,
+      tasks: tasks,
+    });
+
+  
+    form.style.display = "none";
+  });
+}
