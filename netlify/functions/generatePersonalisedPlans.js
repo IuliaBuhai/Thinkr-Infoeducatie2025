cb0dd60eef03ee79d741337203d0686713d417f6
IuliaBuhai <137499884+IuliaBuhai@users.noreply.github.com>
2025-07-26 16:14:37 +0300

    prompt planuri

--- netlify/functions/generatePersonalisedPlans.js
+++ netlify/functions/generatePersonalisedPlans.js
@@ -41,7 +41,7 @@ Creează un plan detaliat de studiu pentru această săptămână. Planul trebui
 
 - Fiecare zi va fi etichetată astfel: ${weekDaysOrdered.join(", ")}.Folosește exact aceste etichete în planul returnat pentru a ajuta la organizarea după dată. ,Asociază corect task-urile cu ziua potrivită și nu schimba ordinea.;
 - Pentru fiecare zi, include 2–5 task-uri;
-- Pentru fiecare task, include: titlu, descriere detaliată(care sa contina si metoda de studiu sugerata in functie de ce tip de invatare i se potrivete elevului), durată (în minute ), resurse web și cărți, sau "nu e nevoie" daca nu e cazul ;
+- Pentru fiecare task, include: titlu, descriere scurtă, dar detaliată(care sa contina si metoda de studiu sugerata in functie de ce tip de invatare i se potrivete elevului), durată (în minute ), resurse web și cărți, sau "nu e nevoie" daca nu e cazul ;
 - Studentul are ${age} ani, stilul său de învățare optim este: "${learnerType}";
 - Are următoarele task-uri de finalizat (în ordinea priorității): ${tasks}, unele task-uri au detalii în paranteze, deci ai grijă să ți cont de ele ;
 - Organizează sarcinile eficient și echilibrat.
